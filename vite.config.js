8ca7ea545f7655dab5d8ddc43fcc28e29d48eb1d
theprocesswallah <theprocesswallah@gmail.com>
2026-07-23 11:09:59 +0530

    Setup Vite development environment

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        products: resolve(__dirname, 'products.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+})
